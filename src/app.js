eabbf6427be3d070ca2df30e9a122565d8911c42
Sandip Roy <sandiproyofficial29@gmail.com>
2026-05-12 12:52:02 +0530

    feat: add request logger middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import healthRoute from "../src/routes/health.route.js"
+import requestLogger from "./middlewares/requestLogger.js";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(requestLogger)
 
 app.use("/health", healthRoute);
 

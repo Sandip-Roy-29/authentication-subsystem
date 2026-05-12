9a89458abd1198dd06691408d09759e0d8cd20af
Sandip Roy <sandiproyofficial29@gmail.com>
2026-05-12 20:15:36 +0530

    refactor: separate requestId generation into dedicated middleware

---
+++ src/middlewares/requestIdMiddleware.js
@@ -0,0 +1,12 @@
+import { randomUUID } from "crypto";
+
+const requestIdMiddleware = (req, res, next) => {
+    const requestId = randomUUID();
+    req.requestId = requestId;
+
+    res.setHeader("X-Request=Id", requestId);
+
+    next();
+};
+
+export default requestIdMiddleware;
--- src/middlewares/requestLogger.js
+++ src/middlewares/requestLogger.js
@@ -1,37 +1,31 @@
 import logger from "../utils/logger.js";
-import crypto from "crypto";
 
 const requestLogger = (req, res, next) => {
     const start = Date.now();
-
-    const requestId = crypto.randomUUID();
-    req.requestId = requestId;
-
-    res.setHeader("X-Request=Id", requestId);
 
     res.on("finish", () => {
         const responseTime = Date.now - start;
 
         const logData = {
-            requestId,
+            requestId: req.requestId,
             method: req.method,
             route: req.originalUrl,
             statuscode: req.statuscode,
             responseTime: `${responseTime}ms`,
             ip: req.ip,
-            userAgent: req.headers["user-agent"]
+            userAgent: req.headers["user-agent"],
         };
-            
-        if(res.statuscode >= 500){
-            logger.error(logData,"Server error response")
-        } else if(res.statuscode >= 400){
-            logger.warn(logData,"Client error response")
-        } else{
-            logger.info(logData,"Request completed")
-        }
-    })
 
-    next()
-}
+        if (res.statuscode >= 500) {
+            logger.error(logData, "Server error response");
+        } else if (res.statuscode >= 400) {
+            logger.warn(logData, "Client error response");
+        } else {
+            logger.info(logData, "Request completed");
+        }
+    });
+
+    next();
+};
 
 export default requestLogger;
